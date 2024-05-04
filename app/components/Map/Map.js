6f4bef6e2b7b5432f913aece297f16fc9e61035d
JamesCooper97 <james.cooper97@outlook.com>
2024-05-04 02:44:28 +0100

    Replace OS map with countries polygons

--- app/components/Map/Map.js
+++ app/components/Map/Map.js
@@ -1,9 +1,10 @@
 "use client"
 import 'leaflet/dist/leaflet.css';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import style from '../../styles/Home.module.css';
 import Script from 'next/script';
-import * as countryData from "public/Data/countries_polygons.js";
+import * as countriesData from './countries_polygons.json';
 
 import * as L from 'leaflet';
 
@@ -11,15 +12,7 @@ export default function Map(){
     //fix for "Map already instantiated"
     document.getElementById('weathermap').innerHTML = "<div id='map' style='width: 100%; height: 600px;'></div>";
     const map = L.map('map').setView([37.8, -96], 4);
-    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '© OpenStreetMap' }).addTo(map);
+    //const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '© OpenStreetMap' }).addTo(map);
     console.log("Base Map Initialised");
-    L.geoJson(countryData.geoJson).addTo(map);
-}
-
-function addGeoJSON(){
-    return(
-        <script>
-
-        </script>
-    );
+    L.geoJson(countriesData).addTo(map);
 }
